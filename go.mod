aa0d5e4cee67047034a95ea578e40567e96554d1
Deja Clarke (dejclark) <dejclark@cisco.com>
2022-12-08 13:32:46 -0800

    files set up

---
+++ go.mod
@@ -0,0 +1,8 @@
+module myApp
+
+go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"net/http"
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+	mgo "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Database
+
+func main() {
+	
+}
